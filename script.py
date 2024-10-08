4fac0ef22d3f481bca582503e6f8074c131aaca8
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2024-10-07 19:35:51 -0500

    Create script.py

---
+++ script.py
@@ -0,0 +1,20 @@
+import requests, pandas as pd
+from bs4 import BeautifulSoup as bsoup
+
+session = requests.Session()
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Host": "app.servir.gob.pe",
+    "Sec-Fetch-User": "?1",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Dest": "document",
+}
+response=session.get("https://app.servir.gob.pe/DifusionOfertasExterno/faces/consultas/ofertas_laborales.xhtml", headers = HEADERS)
+print(response)
